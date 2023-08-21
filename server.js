fd648da40a66ce8495f2e989669de7af5ccbd851
ANIT SAHA <118753732+ForbiddenLinker14@users.noreply.github.com>
2023-08-21 14:00:05 +0530

    server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 // Initialize Express app
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5600;
 
 // Configure multer for file upload
 const upload = multer();
